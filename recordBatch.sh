39ac6364e31b1b860d7b5c297b7266e1f119209c
RolandWarburton <warburtonroland@gmail.com>
2021-10-23 23:12:14 +1100

    Bunch of stuff

--- recordBatch.sh
+++ recordBatch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Save 15 min chunks (900s) from the camera
 # Make sure to change the variables below to work with your camera
@@ -8,8 +8,18 @@ PASSWORD="password"
 ADDRESS="192.168.0.135:554"
 SCHEME="h264Preview_01_sub"
 
+# h264Preview_01_sub
+WIDTH=640
+HEIGHT=480
+FPS=15
+
+# h264Preview_01_main
+# WIDTH=
+# HEIGHT=
+# FPS=
+
 # rtsp://admin:password@192.168.0.135:554//h264Preview_01_sub
-STREAM_URL="rtsp://$USERNAME:$PASSWORD@$ADDRESS//$SCHEME"
+STREAM_URL="rtsp://$USERNAME:$PASSWORD@$ADDRESS/$SCHEME"
 
 # the name of this camera
 # used to create sub directories for writing footage to
@@ -23,7 +33,19 @@ mkdir logs 2> /dev/null
 # move to the 15 min directory
 cd 15min/$CAM_NAME
 
-openRTSP -n -c -D 5 -4 -w 640 -h 480 -f 15 -F $CAM_NAME -P 60 -u admin password $STREAM_URL
+
+openRTSP \
+-n \
+-c \
+-D 25 \
+-4 \
+-w $WIDTH \
+-h $HEIGHT \
+-f $FPS \
+-F $CAM_NAME \
+-P 900 \
+-u $USERNAME $PASSWORD \
+$STREAM_URL
 
 # http://www.live555.com/openRTSP/
 # -n								be notified when RTP data packets start arriving
--- runHourly.sh
+++
@@ -1,2 +0,0 @@
-# compress hourly blocks to daily blocks
-./compress.sh -c cam01 -t hourly/cam01 -d daily/cam01 -b 2
